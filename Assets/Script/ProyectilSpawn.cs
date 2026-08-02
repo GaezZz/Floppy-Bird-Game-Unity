81b3f6b64baaf1b8098808abe0b28edcd69187c0
GaezZz <232129347+GaezZz@users.noreply.github.com>
2026-08-02 18:25:40 +0200

    Cambios script proyectil spawn

--- Assets/Script/ProyectilSpawn.cs
+++ Assets/Script/ProyectilSpawn.cs
@@ -1,3 +1,4 @@
+using System;
 using UnityEngine;
 
 public class ProyectilSpawn : MonoBehaviour
@@ -5,6 +6,8 @@ public class ProyectilSpawn : MonoBehaviour
 
     //le digo que prefab tiene que soltar
     public GameObject proyectil;
+
+    public Boolean isDead = false; //si esta en pantalla no puedo tirar mas
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -15,6 +18,11 @@ public class ProyectilSpawn : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        if (isDead == true)
+        {
+            //lo saco en el punto del spawn y el solo con el otro script se mueve
+            Instantiate(proyectil, new Vector3(transform.position.x, transform.position.y, transform.position.z), transform.rotation);
+        }
         
     }
 }
